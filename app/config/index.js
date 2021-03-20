05a4fe13975e5c33459580a3e0c32851c04ebfa1
yb <yangbo101131@163.com>
2021-03-20 10:25:46 +0800

    58

--- app/config/index.js
+++ app/config/index.js
@@ -1,9 +1,4 @@
-const DB_URL = "mongodb://127.0.0.1:27017/dbTest"
-const Redis_URL = {
-    host:'127.0.0.1',
-    port: 6379,
-}
-const secret = "xqyz-jwt-secret"
+
 module.exports = {
     DB_URL,
     secret,
